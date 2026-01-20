c039f61f5f021b8f634ef6a6a92b8f8310f90e0f
Shivang Patel <patelshivang702@gmail.com>
2026-01-20 17:02:45 +0530

    changes in envoy and simulation service

--- dispatch-manager-service/src/test/java/com/project/hems/dispatch_manager_service/DispatchManagerServiceApplicationTests.java
+++ dispatch-manager-service/src/test/java/com/project/hems/dispatch_manager_service/DispatchManagerServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.hems.dispatch_manager_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DispatchManagerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
