6a769549c084a1c794bea9246c694b4df2677f45
Shivang Patel <patelshivang702@gmail.com>
2026-01-20 12:36:33 +0530

    initialized dispatch manager service

---
+++ dispatch-manager-service/src/main/java/com/project/hems/dispatch_manager_service/DispatchManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.hems.dispatch_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchManagerServiceApplication.class, args);
+	}
+
+}
---
+++ dispatch-manager-service/src/test/java/com/project/hems/dispatch_manager_service/DispatchManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hems.dispatch_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
