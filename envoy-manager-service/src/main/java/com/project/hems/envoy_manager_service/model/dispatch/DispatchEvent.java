7bc892877a0e51d5a327e396f8026728288243b1
Shivang Patel <patelshivang702@gmail.com>
2026-01-28 20:05:37 +0530

    removed envoy manager submodule and added the envoy gradle project directly

---
+++ envoy-manager-service/src/main/java/com/project/hems/envoy_manager_service/model/dispatch/DispatchEvent.java
@@ -0,0 +1,47 @@
+package com.project.hems.envoy_manager_service.model.dispatch;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.project.hems.envoy_manager_service.model.EnergyPriority;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DispatchEvent {
+
+    @NotNull(message = "dispatchId cannot be null")
+    private Long dispatchId;
+
+    @NotNull(message = "siteId cannot be null")
+    private UUID siteId;
+
+    @NotNull(message = "eventType cannot be null")
+    private DispatchEventType eventType;
+
+    @NotNull(message = "powerReqW cannot be null")
+    @Positive(message = "powerReqW must be greater than 0")
+    private Long powerReqW;
+
+    @NotNull(message = "durationSec cannot be null")
+    @Positive(message = "durationSec must be greater than 0")
+    private Long durationSec;
+
+    @NotEmpty(message = "energyPriority cannot be empty")
+    private List<@NotNull EnergyPriority> energyPriority;
+
+    @Size(min = 1, max = 255, message = "reason must be at most 255 characters")
+    private String reason;
+}
---
+++ envoy-manager-service/src/test/java/com/project/hems/envoy_manager_service/EnvoyManagerServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.project.hems.envoy_manager_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvoyManagerServiceApplicationTests {
+
+}
