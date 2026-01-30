4313c4171e0a2c4d862ef9aa36f0eb6eb1908b51
hems <hems07293@gmail.com>
2026-01-30 15:59:26 +0530

    add new endpoint /fetch-all-site-by-region in siteService

--- Virtual-Power-Plant/src/main/java/com/hems/project/Virtual_Power_Plant/Config/WebSocketConfig.java
+++ Virtual-Power-Plant/src/main/java/com/hems/project/Virtual_Power_Plant/Config/WebSocketConfig.java
@@ -10,16 +10,26 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+     @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");//ahiya message broadcast thase 
+        //kafka mathi live message avse e broker ma avse first and broker ma specific aa desitnatioin per avse /topic
+        registry.setApplicationDestinationPrefixes("/app");
+        //this is our endpoint url means user access karvu hoy toh 
+        // user hit karvu padse /app/<apda-controller-no-url>
+        //so ene first /app toh nakhvu j padse
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") //this endpoint client use karse connection established karva 
+                .setAllowedOriginPatterns("*") //this we * because we dont no use host and port so ..
+                .withSockJS(); //this is fallback method becuase some browser dont support websocket 
     }
+
+   
 }
+
+
+//registry.enableSimpleBroker("/topic"); this is for broadcasting messaging
+//registry.setApplicationDestinationPrefixes("/app"); and this is for client to access our endpoint
